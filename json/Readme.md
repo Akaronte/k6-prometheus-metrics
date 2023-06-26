7fcf5656740f294e2859b889321cda0905922ec8
Adrian De Palma <adrian.de.palma.osses.ext@ing.com>
2023-06-26 12:05:20 +0200

    k6_vus

---
+++ json/Readme.md
@@ -0,0 +1,4 @@
+curl "http://localhost:7979/probe?module=animals&target=http://192.168.1.6:9090 
+
+http://localhost:7979/probe?target=http://192.168.1.6:9090
+
--- json/node/server.js
+++ json/node/server.js
@@ -6,22 +6,62 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     //Create a JSON
 	let json_response = [
+		// {
+		//   "noun": "lion",
+		//   "population": 123,
+		//   "predator": true
+		// },
+		// {
+		//   "noun": "deer",
+		//   "population": 456,
+		//   "predator": false
+		// },
+		// {
+		//   "noun": "pigeon",
+		//   "population": 789,
+		//   "predator": false
+		// },
 		{
-		  "noun": "lion",
-		  "population": 123,
-		  "predator": true
-		},
-		{
-		  "noun": "deer",
-		  "population": 456,
-		  "predator": false
-		},
-		{
-		  "noun": "pigeon",
-		  "population": 789,
-		  "predator": false
+			"val": 7,
+			"status": "success",
+			"data": {
+				"resultType": 8,
+				"result": [
+					{
+						"val": 9
+					},
+					{
+						"metric": {
+							"__name__": "k6_vus",
+							"testid": "k6-111"
+						},
+						"value": [
+							1687768382.135,
+							"1"
+						]
+					}
+				]
+			}
 		}
 	  ]
+	// let json_response = {
+	// 	"status": "success",
+	// 	"data": {
+	// 		"resultType": "vector",
+	// 		"result": [
+	// 			{
+	// 				"metric": {
+	// 					"__name__": "k6_vus",
+	// 					"testid": "k6-111"
+	// 				},
+	// 				"value": [
+	// 					1687768382.135,
+	// 					"1"
+	// 				]
+	// 			}
+	// 		]
+	// 	}
+	// }
 	console.log('Server Running');
 	res.end( JSON.stringify(json_response) ); 
 }).listen(9090);
